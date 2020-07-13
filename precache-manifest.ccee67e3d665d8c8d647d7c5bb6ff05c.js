0061a890f768b0c370e4c77cbd21d494c7e20018
Hazuwall <yalutorovsk4@gmail.com>
2020-07-13 12:31:11 +0500

    Updates

---
+++ precache-manifest.ccee67e3d665d8c8d647d7c5bb6ff05c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515e10dd707cc3840a521802f1c72546",
+    "url": "/DetectiveBoard/index.html"
+  },
+  {
+    "revision": "92491104542512c736c9",
+    "url": "/DetectiveBoard/static/css/main.0a56ce45.chunk.css"
+  },
+  {
+    "revision": "03ec2f28ad5d3fca98e3",
+    "url": "/DetectiveBoard/static/js/2.8416e357.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DetectiveBoard/static/js/2.8416e357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92491104542512c736c9",
+    "url": "/DetectiveBoard/static/js/main.f4a9045d.chunk.js"
+  },
+  {
+    "revision": "f40c74853fe9344b99e4",
+    "url": "/DetectiveBoard/static/js/runtime-main.b974a58f.js"
+  }
+]);
